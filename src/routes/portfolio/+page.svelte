a03b55d92912b103a6b04704a2c25b04acd0d29f
William Leader <83631101+wdlea@users.noreply.github.com>
2025-09-13 16:25:48 +1200

    More deployment tweaks

--- src/routes/portfolio/+page.svelte
+++ src/routes/portfolio/+page.svelte
@@ -2,30 +2,9 @@
     import Section from "$lib/Section.svelte";
     import PortfolioItem from "./PortfolioItem.svelte";
     import ScreenshotThis from "$lib/assets/Screenshot This.png";
-    import Flame from "$lib/assets/flame.svelte";
 </script>
 
 <Section title="So, what can you do?">
-    <div class="fill-sky-500">
-        <Flame />
-    </div>
-
-    <PortfolioItem title="This Website" img={ScreenshotThis} href={"/"}>
-        <p>
-            This is a simple static site incorporating page transitions, video
-            backgrounds and a monochrome palette.
-        </p>
-        <p>
-            This is what I can create with free reign over the styling of the
-            site.
-        </p>
-        <p>
-            This site is also responsive, this means that it is designed for a
-            range of screens and will automatically update it's layout to fit.
-            If you're on laptop, you can try this out by resizing the window, on
-            mobile, you could rotate your screen.
-        </p>
-    </PortfolioItem>
     <PortfolioItem title="This Website" img={ScreenshotThis} href={"/"}>
         <p>
             This is a simple static site incorporating page transitions, video
