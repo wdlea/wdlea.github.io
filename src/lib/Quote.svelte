159765002de6367ceb05e39314519054457eedfd
William Leader <83631101+wdlea@users.noreply.github.com>
2025-10-08 18:21:45 +1300

    Added PPSL testimonial

--- src/lib/Quote.svelte
+++ src/lib/Quote.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    let { source, children }: { source?: string | undefined; children: any } =
+    let { source, children, lightMark }: { source?: string | undefined; children: any; lightMark?: boolean | undefined } =
         $props();
 </script>
 
@@ -7,7 +7,7 @@
     <div class="italic mx-3">
         {@render children?.()}
     </div>
-    <div class="absolute -left-1 -top-3 text-9xl text-sky-400 font-serif -z-10 *:size-20 size-0">
+    <div class={`absolute -left-1 -top-3 text-9xl ${lightMark ? "text-sky-200" : "text-sky-400"} font-serif -z-10 *:size-20 size-0`}>
         "
     </div>
 
--- src/routes/portfolio/+page.svelte
+++ src/routes/portfolio/+page.svelte
@@ -3,15 +3,36 @@
     import PortfolioItem from "./PortfolioItem.svelte";
     import ScreenshotThis from "$lib/assets/Screenshot This.png";
     import ScreenshotNursingChampioNZ from "$lib/assets/Screenshot Nursing ChampionNZ.png";
+    import ScreenshotPPSL from "$lib/assets/Screenshot PPSL.png";
     import Quote from "$lib/Quote.svelte";
 </script>
 
 <Section title="So, what can you do?">
-    <p>
-        This site was only launched on 13 September 2025, I am currently in the
-        process of getting references from previous clients. Please check back
-        in about a week for more.
-    </p>
+    <PortfolioItem title="PPSL" img={ScreenshotPPSL} href={"https://ppsl.nz"}>
+        <p>This was my very first professional website.</p>
+
+        <Quote
+            lightMark
+            source="Richard Peach - General Manager
+                Personnel Profile Specialists Limited"
+        >
+            <p>
+                I can wholeheartedly endorse William Leader if you are looking
+                to develop a web site. He very quickly grasped what we were
+                looking to achieve with our site, which was an initial static
+                site followed by a second stage full access database driven
+                facility. He made numerous suggestions regarding functionality
+                and security as well as platform suggestions to ensure the
+                second stage would function properly.
+            </p>
+            <p>
+                He is fast with the work but thorough at the same time and gives
+                great value for money. You can be confident in the quality of
+                what he will produce for you.
+            </p>
+        </Quote>
+    </PortfolioItem>
+
     <PortfolioItem
         title="Nursing ChampioNZ"
         img={ScreenshotNursingChampioNZ}
@@ -23,8 +44,9 @@
             <p>
                 William has worked with me over the past year to set up my
                 website and publish interviews and other items on my website
-                <a href="https://www.nursingchampionz.nz" class="underline text-sky-500"
-                    >www.nursingchampionz.nz</a
+                <a
+                    href="https://www.nursingchampionz.nz"
+                    class="underline text-sky-500">www.nursingchampionz.nz</a
                 >. He has been patient, listened carefully, offered advice and
                 delivered in a timely manner. I am so pleased with his work and
                 highly recommend him.
